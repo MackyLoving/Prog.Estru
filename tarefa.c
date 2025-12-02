f4524e620995518c201f19ebfa04639f0e492437
MackyLoving <159178703+MackyLoving@users.noreply.github.com>
2025-12-02 11:19:06 -0300

    Update tarefa.c
    Função em c que retorna um fatorial:

--- tarefa.c
+++ tarefa.c
@@ -1,1 +1,19 @@
-uyfyftgvyu
+void fatorial(int n, int *resultado) {
+    *resultado = 1; 
+
+    for (int i = 2; i <= n; i++) {
+        *resultado *= i;  
+    }
+}
+
+int main() {
+    int num = 5;      
+    int fat;        
+
+    fatorial(num, &fat);  
+
+   
+    printf("Fatorial de %d = %d\n", num, fat);
+
+    return 0;  
+}
